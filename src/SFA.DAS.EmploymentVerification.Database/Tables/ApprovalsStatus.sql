ca537b2571b51a64156feb59a2ad897e08daa735
Supriya <supriya.kolate@education.gov.uk>
2024-09-24 14:33:08 +0100

    Removed Identity constraint from master data tables as all the status could not be in sequnetial order

--- src/SFA.DAS.EmploymentVerification.Database/Tables/ApprovalsStatus.sql
+++ src/SFA.DAS.EmploymentVerification.Database/Tables/ApprovalsStatus.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[ApprovalsStatus]
 (
-  ApprovalsStatusId SMALLINT NOT NULL Identity(1,1) CONSTRAINT PK_ApprovalsStatus PRIMARY KEY,
+  ApprovalsStatusId SMALLINT NOT NULL CONSTRAINT PK_ApprovalsStatus PRIMARY KEY,
   Approvals VARCHAR(100)
 )
--- src/SFA.DAS.EmploymentVerification.Database/Tables/CommitmentStatus.sql
+++ src/SFA.DAS.EmploymentVerification.Database/Tables/CommitmentStatus.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[CommitmentStatus]
 (
-  CommitmentStatusId SMALLINT NOT NULL Identity(1,1) CONSTRAINT PK_CommitmentStatus PRIMARY KEY,
+  CommitmentStatusId SMALLINT NOT NULL CONSTRAINT PK_CommitmentStatus PRIMARY KEY,
   CommitmentStatus VARCHAR(20)
 );
 GO
